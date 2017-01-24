36a6b496e1ad06c680f93f41e4cac9721ebfb92f
Steve Licata <Steve.Licata@gmail.com>
2017-01-24 10:19:21 -0500

    layouts, js, images

--- Agency-week-application/app/assets/javascripts/application.js
+++ Agency-week-application/app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+document.addEventListener('turbolinks:load', function() {
+
+console.log("loaded");
+
+var ham = document.getElementById("hamburger");
+ham.addEventListener('click', function(){
+  openNav();
+});
+
+var closex = document.getElementById("closingx");
+closex.addEventListener('click', function(){
+  closeNav();
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+};
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+};
+
+
+});//end turbolinks load
